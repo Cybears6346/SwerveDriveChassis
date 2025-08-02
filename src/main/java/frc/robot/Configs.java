d248ffbcf6d562339fc89ed3b1ae9a2f54a3ac2d
Cybears6346 <195391145+Cybears6346@users.noreply.github.com>
2025-08-02 15:10:20 -0400

    error msg

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -76,3 +76,50 @@ public final class Configs {
         }
     }
 }
+
+
+// determine where the error occurred. ﻿
+// ﻿﻿﻿﻿﻿﻿   See https://wpilib.org/stacktrace for more information. ﻿
+// ﻿﻿﻿﻿﻿﻿ Error at edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:440): The startCompetition() method (or methods called by it) should have handled the exception above. ﻿
+// ﻿﻿﻿﻿﻿﻿ ********** Robot program starting ********** ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: Listening on NT3 port 1735, NT4 port 5810 ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: Got a NT4 connection from 10.63.46.20 port 63866 ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: CONNECTED NT4 client 'Dashboard@1' (from 10.63.46.20:63866) ﻿
+// ﻿﻿﻿﻿﻿﻿﻿﻿ERROR ﻿﻿ 1 ﻿﻿ Failed to load RobotConfig from GUI settings: /home/lvuser/deploy/pathplanner/settings.json (No such file or directory) ﻿﻿ frc.robot.RobotContainer.<init>(RobotContainer.java:130) ﻿﻿﻿
+// ﻿﻿﻿﻿﻿﻿ Error at frc.robot.RobotContainer.<init>(RobotContainer.java:130): Failed to load RobotConfig from GUI settings: /home/lvuser/deploy/pathplanner/settings.json (No such file or directory) ﻿
+// ﻿﻿﻿﻿﻿﻿ java.io.FileNotFoundException: /home/lvuser/deploy/pathplanner/settings.json (No such file or directory) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at java.base/java.io.FileInputStream.open0(Native Method) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at java.base/java.io.FileInputStream.open(Unknown Source) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at java.base/java.io.FileInputStream.<init>(Unknown Source) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at java.base/java.io.FileReader.<init>(Unknown Source) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at com.pathplanner.lib.config.RobotConfig.fromGUISettings(RobotConfig.java:266) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.RobotContainer.<init>(RobotContainer.java:110) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.Robot.robotInit(Robot.java:31) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.TimedRobot.startCompetition(TimedRobot.java:107) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:419) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.RobotBase.startRobot(RobotBase.java:510) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.Main.main(Main.java:23) ﻿
+// ﻿﻿﻿﻿﻿﻿﻿﻿ERROR ﻿﻿ 1 ﻿﻿ Unhandled exception: com.pathplanner.lib.auto.AutoBuilderException: AutoBuilder was not configured before attempting to load a PathPlannerAuto from file ﻿﻿ com.pathplanner.lib.commands.PathPlannerAuto.<init>(PathPlannerAuto.java:78) ﻿﻿﻿
+// ﻿﻿﻿﻿﻿﻿ Error at com.pathplanner.lib.commands.PathPlannerAuto.<init>(PathPlannerAuto.java:78): Unhandled exception: com.pathplanner.lib.auto.AutoBuilderException: AutoBuilder was not configured before attempting to load a PathPlannerAuto from file ﻿
+// ﻿﻿﻿﻿﻿﻿﻿﻿Warning ﻿﻿ 1 ﻿﻿ The robot program quit unexpectedly. This is usually due to a code error.
+//   The above stacktrace can help determine where the error occurred.
+//   See https://wpilib.org/stacktrace for more information. ﻿﻿ edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:433) ﻿﻿﻿
+// ﻿﻿﻿﻿﻿﻿ 	at com.pathplanner.lib.commands.PathPlannerAuto.<init>(PathPlannerAuto.java:78) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at com.pathplanner.lib.commands.PathPlannerAuto.<init>(PathPlannerAuto.java:63) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.RobotContainer.<init>(RobotContainer.java:136) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.Robot.robotInit(Robot.java:31) ﻿
+// ﻿﻿﻿﻿﻿﻿﻿﻿ERROR ﻿﻿ 1 ﻿﻿ The startCompetition() method (or methods called by it) should have handled the exception above. ﻿﻿ edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:440) ﻿﻿﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.TimedRobot.startCompetition(TimedRobot.java:107) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:419) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at edu.wpi.first.wpilibj.RobotBase.startRobot(RobotBase.java:510) ﻿
+// ﻿﻿﻿﻿﻿﻿ 	at frc.robot.Main.main(Main.java:23) ﻿
+// ﻿﻿﻿﻿﻿﻿  ﻿
+// ﻿﻿﻿﻿﻿﻿ ﻿Warning﻿ at edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:433): The robot program quit unexpectedly. This is usually due to a code error. ﻿
+// ﻿﻿﻿﻿﻿﻿   The above stacktrace can help determine where the error occurred. ﻿
+// ﻿﻿﻿﻿﻿﻿   See https://wpilib.org/stacktrace for more information. ﻿
+// ﻿﻿﻿﻿﻿﻿ Error at edu.wpi.first.wpilibj.RobotBase.runRobot(RobotBase.java:440): The startCompetition() method (or methods called by it) should have handled the exception above. ﻿
+// ﻿﻿﻿﻿﻿﻿ ********** Robot program starting ********** ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: Listening on NT3 port 1735, NT4 port 5810 ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: Got a NT4 connection from 10.63.46.20 port 63881 ﻿
+// ﻿﻿﻿﻿﻿﻿ NT: CONNECTED NT4 client 'Dashboard@1' (from 10.63.46.20:63881) ﻿
+// ﻿﻿﻿﻿﻿﻿﻿﻿ERROR ﻿﻿ 1 ﻿﻿ Failed to load RobotConfig from GUI settings: /home/lvuser/deploy/pathplanner/settings.json (No such file or directory) ﻿﻿ frc.robot.RobotContainer.<init>(RobotContaine
